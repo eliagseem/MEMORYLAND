12fc438223aeabbc4e95a226f3c31059f2b2b87c
Ali Egseem <aliegseem@gmail.com>
2019-10-22 02:40:52 -0400

    Can raycast detect when an NPC is clicked and stops navmesh wandering

---
+++ GAME/MEMORYGARDEN/Assets/Scripts/camcastScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camcastScript : MonoBehaviour
+{
+    public Camera camera;
+
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+
+                if(objectHit.gameObject.tag == "NPC")
+                {
+                    //clicked on NPC
+                    //make NPC face player
+                    //play their dialogue audio clip
+                    var agent = objectHit.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+                    agent.isStopped = true;
+
+                }
+            }
+        }
+    }
+}
