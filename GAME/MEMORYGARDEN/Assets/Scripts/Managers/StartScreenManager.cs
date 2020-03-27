2010e47811cf4505ae45ad97f61727ef893ea71d
Ali <aliegseem@gmail.com>
2020-03-26 23:12:19 -0400

    Add start screen, main menu, begin working on main menu selection

---
+++ GAME/MEMORYGARDEN/Assets/Scripts/Managers/StartScreenManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenManager : MonoBehaviour
+{
+    public AudioSource soundSource;
+    public AudioClip selectionSound;
+
+    private float timer;
+    private bool selectionMade = false;
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            soundSource.clip = selectionSound;
+            soundSource.Play();
+            selectionMade = true;
+
+            //change text animation for press start
+        }
+
+        //wait for the sound effect and animation to finish
+        if (selectionMade)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer >= 6)
+        {
+            //load in second scene
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+}
